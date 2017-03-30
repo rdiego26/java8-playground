012c219e4236478e13f153f7b6b6b58d12b6c235
ramos <rdiego26@gmail.com>
2017-03-30 07:07:20 -0300

    Optimize with method reference

--- java8/src/OrdenaStrings.java
+++ java8/src/OrdenaStrings.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class OrdenaStrings {
 
     public static void main(String[] args) {
 
-        List<String> palavras = new ArrayList<String>();
+        List<String> palavras = new ArrayList<>();
 
         palavras.add("uva");
         palavras.add("morango");
         palavras.add("maracuja");
         palavras.add("abacaxi");
 
-        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+        palavras.sort(Comparator.comparing(String::length));
         palavras.forEach(s -> System.out.println(s));
     }
 
