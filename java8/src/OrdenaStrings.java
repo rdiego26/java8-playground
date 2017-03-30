48ac95bb4c4969f31e6afec7dd713d232bdf2ada
ramos <rdiego26@gmail.com>
2017-03-30 06:43:45 -0300

    Implement lambda to replace ComparadorPorTamanho

--- java8/src/OrdenaStrings.java
+++ java8/src/OrdenaStrings.java
@@ -1,38 +1,19 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class OrdenaStrings {
 
     public static void main(String[] args) {
 
         List<String> palavras = new ArrayList<String>();
-        Comparator<String> comparator = new ComparadorPorTamanho();
 
         palavras.add("uva");
         palavras.add("morango");
         palavras.add("maracuja");
         palavras.add("abacaxi");
 
-        palavras.sort(comparator);
+        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
         palavras.forEach(s -> System.out.println(s));
     }
 
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-
-    @Override
-    public int compare(String o1, String o2) {
-
-        if(o1.length() < o2.length())
-            return -1;
-
-        if(o1.length() > o2.length())
-            return 1;
-
-        return 0;
-    }
 }
